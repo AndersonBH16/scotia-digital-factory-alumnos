30c76d9e5439a760386e1a837286ad9a67e26ace
Anderson Blas <andi_1516@hotmail.com>
2026-07-18 19:15:11 -0500

    Setup base project

---
+++ src/main/java/com/scotiabank/digital_factory_alumnos/DigitalFactoryAlumnosApplication.java
@@ -0,0 +1,13 @@
+package com.scotiabank.digital_factory_alumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFactoryAlumnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalFactoryAlumnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scotiabank/digital_factory_alumnos/DigitalFactoryAlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scotiabank.digital_factory_alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalFactoryAlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
